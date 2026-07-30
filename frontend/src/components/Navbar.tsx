7ddfb9d26b1f5660dfad43c87f7c2a7832f8dcc7
Nikhil42865 <nikhil42865@gmail.com>
2026-07-30 23:00:31 +0530

    wrote the global css file with it's content

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,34 @@
+import "./Navbar.css";
+
 function Navbar() {
     return (
         <nav className="navbar">
-            <h1>Navbar</h1>
+
+            <div className="navbar-left">
+                <h2>InterviewForgeAI</h2>
+            </div>
+
+            <div className="navbar-center">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                />
+            </div>
+
+            <div className="navbar-right">
+
+                <button>
+                    🔔
+                </button>
+
+                <div className="profile">
+                    NK
+                </div>
+
+            </div>
+
         </nav>
-    )
+    );
 }
 
 export default Navbar;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
 import './index.css'
 import App from './App.tsx'
+import "./styles/variables.css"
+import "./styles/globals.css";
+import "./styles/typography.css";
+import "./styles/animations.css";
+import "./styles/utilities.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
